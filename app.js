aa1af2d646ede1ce8dfe40e5fda61f4740dd69a0
Joe Douglas <jdouglas8@ivytech.edu>
2023-02-28 18:36:03 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+//express app
+const app = express();
+
+//reqister view engine
+app.set('view engine', 'ejs');
+
+// listen for requests
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem Ipsum Dolor Sit Amet Consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem Ipsum Dolor Sit Amet Consectetur'},
+        {title: 'How to defeat Bowser', snippet: 'Lorem Ipsum Dolor Sit Amet Consectetur'},
+    ];
+
+    //res.send('<p>home page</p>');
+    res.render('index', {title: 'Home', blogs});
+});
+
+app.get('/about', (req, res) => {
+
+    //res.send('<p>about page</p>');
+    res.render('about', {title: 'About'});
+});
+
+/* //redirects
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+}); */
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', {title: 'Create a new blog'});
+})
+
+// 404 page
+app.use((req, res) => {
+    res.status(404).render('404', {title: '404'});
+});
---
+++ test.js
@@ -0,0 +1,10 @@
+// const name = "yoshi";
+
+// console.log(name);
+
+//function greet(name) {
+ ///   console.log('hello, ' + name);
+//}
+
+//greet('mario');
+//greet('yoshi');
