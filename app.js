8fa594f99d5e85ee9f4d08caba61a22a7ab1c3d4
Joe Douglas <jdouglas8@ivytech.edu>
2023-03-01 14:35:57 -0600

    Added Mongoose, MongoDB Atlas - JD

--- app.js
+++ app.js
@@ -1,30 +1,65 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
-//express app
+// express app
 const app = express();
+
+// connect to mongodb
+const dbURI = 'mongodb+srv://netninja:testtest1234@nodetuts.zgahmqy.mongodb.net/nodetuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
 
 //reqister view engine
 app.set('view engine', 'ejs');
 
 // listen for requests
-app.listen(3000);
+// app.listen(3000);
 
 // middleware and static files
 
 app.use(express.static('public'));
-
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem Ipsum Dolor Sit Amet Consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem Ipsum Dolor Sit Amet Consectetur'},
-        {title: 'How to defeat Bowser', snippet: 'Lorem Ipsum Dolor Sit Amet Consectetur'},
-    ];
+/* mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) =>{
+    const blog = new Blog({
+        title: 'new blog 2',
+        snippet: 'about my new blog',
+        body: 'more about my new blog'
+    });
 
-    //res.send('<p>home page</p>');
-    res.render('index', {title: 'Home', blogs});
+    blog.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+})
+
+app.get('/all-blogs', (req, res) => {
+    Blog.find()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+})
+
+app.get('/single-blog', (req, res) => {
+    Blog.findById('63fed9d45b5e30241703807d')
+        .then ((result) => {
+            res.send(result)
+        })
+}); */
+
+// routes
+app.get('/', (req, res) => {
+    res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
@@ -37,6 +72,18 @@ app.get('/about', (req, res) => {
 app.get('/about-us', (req, res) => {
     res.redirect('/about');
 }); */
+
+// blog routes
+
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1})
+        .then((result) => {
+            res.render('index', {title: 'All Blogs', blogs: result})
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', {title: 'Create a new blog'});
